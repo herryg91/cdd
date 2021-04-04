20d1d1ec630cfb731de89e6bc5a4069f552e1aa3
Herry Gunawan <hg@Herrys-MacBook-Pro.local>
2021-04-04 13:33:44 +0700

    adding api key to default header

--- grst/cors.go
+++ grst/cors.go
@@ -33,7 +33,7 @@ func (c CORSPolicy) getAllOrigins() []string {
 	return resp
 }
 
-var DefaultCORSPreflightHeaders = []string{"Accept", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization",
+var DefaultCORSPreflightHeaders = []string{"Accept", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "ApiKey",
 	grst_context.CONTEXT_CLIENT_APPNAME.String(),
 	grst_context.CONTEXT_CLIENT_APPVERSION.String(),
 	grst_context.CONTEXT_CLIENT_APPVERSIONCODE.String(),
